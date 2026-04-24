ec11a6dbf32db611732ea2a4f2c150343020710b
Aya Majjati <aya.majjati@gmail.com>
2026-04-24 17:48:40 +0100

    changes

--- src/model/Filiere.java
+++ src/model/Filiere.java
@@ -2,10 +2,9 @@ package model;
 
 public class Filiere{
 	private int id , nbEtudiants;
-	private String code , nom ; 
-	public Filiere (int id , int nbEtudiants ,String code ,String nom) {
+	private String nom ; 
+	public Filiere (int id , int nbEtudiants ,String nom) {
 		this.id=id;
-		this.code=code;
 		this.nom=nom;
 		this.nbEtudiants= nbEtudiants ;
 	}
@@ -15,10 +14,21 @@ public class Filiere{
 	public String getNom() {
 		return nom;
 	}
-	public String getCode() {
-		return code ;  
-	}
 	public int getNbEtudiants() {
 		return nbEtudiants; 
 	}
+	public void setID(int id) {
+		this.id=id;
+	}
+	public void setNom(String Nom) {
+		this.nom=Nom;
+	}
+	public void setnbEtudiants(int nbEtudiants) {
+		this.nbEtudiants=nbEtudiants ;
+	}
+	@Override
+	public String toString() {
+	    return "Filiere{id=" + id + ", nom='" + nom + "', nbEtudiants=" + nbEtudiants + "}";
+	}
+	
 }
---
+++ src/repository/IdRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import java.util.List;
+
+public interface IdRepository<T> {
+    void sauvegarder(T objet);
+    List<T> chargerTous();
+    T trouverParId(int id);
+    boolean supprimer(int id);
+}
