f918f08fed18e46051661bf17966cbe6ac243d93
oubairhn-cell <ton-email@gmail.com>
2026-04-26 17:08:37 +0200

    h

--- src/algorithm/FilierePlanningStrategy.java
+++ src/algorithm/FilierePlanningStrategy.java
@@ -1,5 +1,27 @@
 package algorithm;
 
+import model.Creneau;
+import model.Enseignant;
+import model.Etudiant;
+import model.Salle;
+import model.Soutenance;
+
+import java.util.List;
+
 public interface FilierePlanningStrategy {
 
+    List<Soutenance> genererPlanning(
+            List<Etudiant>   etudiants,
+            List<Enseignant> enseignants,
+            List<Salle>      salles,
+            List<Creneau>    creneaux
+    );
+
+    default String getNom() {
+        return this.getClass().getSimpleName();
+    }
+
+    default boolean estRealisable(int nbEtudiants, int nbCreneaux, int nbEnseignants) {
+        return nbCreneaux >= nbEtudiants && nbEnseignants >= 2;
+    }
 }
